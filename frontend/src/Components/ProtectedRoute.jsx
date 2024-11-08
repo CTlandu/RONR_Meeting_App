5682c69fb27b245a90ea12e2041dd059102b3c2f
CTlandu <jizhoutang@outlook.com>
2024-11-08 01:11:27 -0500

    [Add] ProtectedRoute.jsx, now pages like Profile are protected unless user logged in

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = document.cookie.includes("token"); // 检查是否有 token
-  console.log("Is Authenticated:", isAuthenticated); // 添加调试信息
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("/api/auth/check-auth", { withCredentials: true })
+      .then((response) => {
+        setIsAuthenticated(true);
+      })
+      .catch((error) => {
+        console.error("Not authenticated:", error.response.data);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // 或者一个加载指示器
+  }
 
   return isAuthenticated ? children : <Navigate to="/login" />;
 };
