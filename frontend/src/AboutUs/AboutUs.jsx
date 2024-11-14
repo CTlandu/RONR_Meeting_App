0d3d101470b5c2fb6addd820df2360aa49b74fa2
Ajax <SuperAjax3270@gmail.com>
2024-11-14 13:43:34 -0500

    Added navigation bar and meeting history page

--- frontend/src/AboutUs/AboutUs.jsx
+++ frontend/src/AboutUs/AboutUs.jsx
@@ -12,16 +12,18 @@ const AboutUs = () => {
       <div className="flex-1 bg-white p-10 relative">
         {/* Header */}
         <div className="absolute top-8 right-0 transform bg-gray-400 p-4 rounded-tl-lg rounded-bl-lg">
-          <h2 className="text-2xl font-bold text-black">About Us - RONR</h2>
+          <h2 className="text-2xl font-bold text-black">About Eon RONR</h2>
         </div>
 
         {/* Footer Link */}
         <div className="absolute bottom-8 left-10">
-          <a href="/prelogin" className="text-black font-bold hover:underline">&larr; back to home page</a>
+          <a href="/prelogin" className="text-black font-bold hover:underline">
+            &larr; back to home page
+          </a>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default AboutUs;
