e533d5e7665439c3cfeeb6bb6d0646b10f3c4320
Duohan Xu <dxu03@wm.edu>
2024-11-14 12:55:07 -0500

    a

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login/Login";
 import Home from "./Home";
 import Webhomepage from "./webhomepage/Webhomepage";
+<<<<<<< HEAD
 import Profile from "./profile/profile";
 import CreateMeeting from "./CreateMeeting/CreateMeeting";
 import ChooseRole from "./choose_role/choose_role";
 import AboutUs from "./AboutUs/AboutUs";
 import ProtectedRoute from "./Components/ProtectedRoute";
+=======
+import Profile from "./profile/profile"
+import Review from "./Review/review";
+>>>>>>> 945d6bb (a)
 
 function App() {
   return (
@@ -17,6 +22,7 @@ function App() {
         <Route path="/" element={<Webhomepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/prelogin" element={<Webhomepage />} />
+<<<<<<< HEAD
         <Route path="/about" element={<AboutUs />} />
         <Route
           path="/profile"
@@ -42,6 +48,10 @@ function App() {
             </ProtectedRoute>
           }
         />
+=======
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/review" element={<Review />} />
+>>>>>>> 945d6bb (a)
       </Routes>
     </Router>
   );
